339df13f52198f1bf52f37adda30791774da5523
ChrisMCodes <34985965+ChrisMCodes@users.noreply.github.com>
2019-10-18 22:35:04 -0700

    A number guessing game

---
+++ higher_lower_guessing.py
@@ -0,0 +1,33 @@
+import random
+
+print('In this game, the computer will pick a number between 1 and 100.')
+print('You will also pick a number.')
+print('If the computer\'s number is lower than yours, this program will tell you so.')
+print('Likewise if the computer\'s number is lower than yours.')
+print('Are you ready to play? Good luck!')
+
+
+def higher_lower(play_num, comp_num):
+    while play_num != comp_num:
+        if play_num > comp_num:
+            print('The computer\'s secret number is lower than yours.')
+            play_num = play_num = int(input('Please enter a number between 1 and {}: '.format(play_num)))
+        elif play_num < comp_num:
+            print('The computer\'s secret number is higher than yours.')
+            play_num = play_num = int(input('Please enter a number between {} and 100: '.format(play_num)))
+    if play_num == comp_num:
+        print('Congratulations! The computer\'s number was {}.'.format(comp_num))
+
+while True:    
+    comp_num = random.randrange(1, 100)
+    play_num = int(input('Please enter a number between 1 and 100: '))
+
+    higher_lower(play_num, comp_num)
+
+    again = input('Would you like to play again? Type YES or NO: ').upper()
+    if again == 'YES' or again == 'Y':
+        print('Good luck!')
+        continue
+    else:
+        print('Thanks for playing!')
+        break
