39efd93e6ecd2bc955f11f48deb68bada0bfa944
ChrisMCodes <34985965+ChrisMCodes@users.noreply.github.com>
2019-11-03 20:13:21 -0700

    New and improved

--- Countdown_clock.py
+++ Countdown_clock.py
@@ -1,24 +1,29 @@
 import time
 from datetime import datetime
 
-now = datetime.now()
-current_time = int(now.strftime('%H%M%S'))
-target = 000000
 
-#This needs cleanup
-while True:
-    if target < current_time: 
-        target = int(input('Input a target time before midnight today in the format "HHMMSS" (please do not use quotes and use the standard 24-hour clock): '))
-    else:
-        break
-#Needs to adjust to subtract from 60
+def countdown():
+    current_hour = datetime.now().hour
+    current_minute = datetime.now().minute
+    current_second = datetime.now().second
+    target_hour = int(input('Input the target HOUR using the 24 hour clock: '))
+    target_minute = int(input('Input the target MINUTE: '))
+    hour_remaining = target_hour - current_hour
+    minute_remaining = target_minute - current_minute
+    second_remaining = 60-current_second
+    while hour_remaining >= 0:
+        while minute_remaining >=0:
+            print('{}:{}:{}'.format(hour_remaining, minute_remaining, second_remaining))
+            time.sleep(1)
+            current_hour = datetime.now().hour
+            current_minute = datetime.now().minute
+            current_second = datetime.now().second
+            hour_remaining = target_hour - current_hour
+            minute_remaining = target_minute - current_minute
+            second_remaining = 60-current_second
+    print('You have reached your goal time!')
 
-print('The current time is: ', current_time)
-print('Now counting down to the time you entered:')
-while target > current_time:
-    print(target - current_time)
-    time.sleep(1)
-    now = datetime.now()
-    current_time = int(now.strftime('%H%M%S'))
+countdown()
+
     
 
