6a8fce8452e344dcbbfd6db44cec83c2fa307bd8
ChrisMCodes <34985965+ChrisMCodes@users.noreply.github.com>
2021-05-12 08:32:28 -0700

    Add files via upload

---
+++ scrapeExample.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+
+website = requests.get('https://quotes.toscrape.com/')
+
+# print(website.text) # .text gets full html of site
+
+
+soup = BeautifulSoup(website.text, 'html.parser')
+
+'''
+title = soup.find('title')
+print(title.text) # .text gets content without tag
+'''
+
+'''
+quote = soup.find(class_='text') # find returns first of its kind
+print(quote.text)
+'''
+'''
+quotes = soup.find_all(class_='text')
+print(quotes) # in iterable list
+'''
+'''
+login_link = soup.find(href='/login')
+print(login_link)
+'''
+
+# Selecting text and author
+quote = soup.find(class_='quote')
+quote_text = quote.find(class_='text')
+quote_author = quote.find(class_='author')
+print(quote.text, quote_author.text)
